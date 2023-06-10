182c3e2da17a84f5fae4df1e56621685ec614c57
Kannakattisanjana <sanjanakannakatti@Sanjanas-MacBook-Air.local>
2023-06-10 11:10:00 -0700

    initial commit

---
+++ Code/scanning_script.py
@@ -0,0 +1,14 @@
+import scokets
+np = nmap.portScanner()
+scan_range = np.scan(hosts="10.0.2.15/502")
+list1 = scan_range['scan']
+for i in range list1:
+	s = socket.scoket.AF_INET, socket.SCOK_STREAM)
+	s.conection(list1, 502)
+	s.send(Message)
+	data = s.recv()
+	if(daya!=null):
+		list1[1] = list2[]
+	s.close()
+print(list2)
+
